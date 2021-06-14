7f66fdb368221bb4c1243c965b6ce5c1eeb78081
Vedurumudi Priyanka <priyankavedurumudi@gmail.com>
2021-06-14 15:56:23 +0530

    Create TextReporter.java

---
+++ TextReporter.java
@@ -0,0 +1,23 @@
+package loa;
+
+/** A Reporter that uses the standard output for messaeges.
+ */
+class TextReporter implements Reporter {
+
+    @Override
+    public void reportError(String fmt, Object... args) {
+        System.err.printf(fmt, args);
+        System.err.println();
+    }
+
+    @Override
+    public void reportNote(String fmt, Object... args) {
+        System.out.printf("* " + fmt, args);
+        System.out.println();
+    }
+
+    @Override
+    public void reportMove(Move move) {
+        System.out.printf("* %s%n", move);
+    }
+}
